99830c7277541e4c03d2cd91627fb1092cda67e4
Todd Rowan <todd@kimandtodd.com>
2013-05-21 17:53:12 -0700

    Initial push

---
+++ js/kandt.js
@@ -0,0 +1,61 @@
+var $ = jQuery;
+(function($) {
+    $(document).ready(function() {
+        $('.cycle_imgs_box').each(function(){$(this).prependTo($(this).parent());});
+        window.setTimeout(function() {$('.cycle_imgs').cycle({
+            fx: 'fade' // choose your transition type, ex: fade, scrollUp, shuffle, etc...
+        });}, 2000);
+    
+        $('.gallery').each(
+            function()
+            {
+                var tos = $('#'+$(this).first().attr('id') +  ' a').TosRUs({"caption":["data-caption"],'anchors':{'zoomIcon':false}});
+                setLoggers(tos);
+            });
+      }
+    )
+ }
+)(jQuery);
+
+
+// This has to be a bug in iOS Safari
+// http://stackoverflow.com/questions/10973649/mobile-safari-reflow-without-resize-buggy-behavior
+function orientation_change() {
+    if (window.orientation == 0 || window.orientation == 180)
+        document.getElementById("viewport").setAttribute("content", "width=device-width, maximum-scale=1.0, initial-scale=1.0, user-scalable=no");
+    else if (window.orientation == -90 || window.orientation == 90)
+        document.getElementById("viewport").setAttribute("content", "width=device-height, maximum-scale=1.0, initial-scale=1.0, user-scalable=no");     
+}
+
+function openLog(e, inx, whatami)
+{
+  // e is the jquery event object
+  // inx is the 0-based index of the slide that's opening. 
+  // This value is only defined if the show opens on a specific slide.
+  // Although undefined is basically = to 0
+  // Not sure about whatami, but I think it's the direct value if set.
+  console.log("Tos opened. Starting image index " + ( typeof inx === 'undefined' ? "undefined, assuming zero": inx) );
+}
+function closeLog(e, duration)
+{
+   console.log("Tos closed. Fadeout duration: " + duration + " ms");
+}
+function slideLog(e, inx, whatami)
+{
+  console.log("Tos sliding. To index: " + inx );
+}
+function loadLog(e, a, div)
+{
+   // e is a jquery event obj. 
+   // a is the $(<A>) element with the image we're loading.
+   // div is the $(<DIV>) element that tos wraps the image in. 
+   if (typeof a !== 'undefined') console.log("Loading image: " + (a[0].pathname).substr((a[0].pathname).lastIndexOf('/')+1));
+}
+
+function setLoggers(tos)
+{
+  tos.bind("opening.tos", openLog);
+  tos.bind("closing.tos", closeLog);
+  tos.bind("sliding.tos", slideLog);
+  tos.bind("loading.tos", loadLog);
+}
---
+++ kimandtodd-photo-gallery.php
@@ -0,0 +1,227 @@
+<?php
+/*
+Plugin Name: Kim and Todd's photo tools
+Plugin URI: http://www.kimandtodd.com
+Description: Incorporates the Awkward Group's slider implementation and cycle lite
+ * for the home page. 
+Version: 0.1
+Author: Todd Rowan
+Author URI: http://www.kimandtodd.com
+License: GPL2
+*/
+
+// Our meta key for excerpt cyclers
+define("GALLERY_META_KEY", "gallery_meta");
+
+// We add a filter to the gallery shortcode and override it. 
+add_filter("post_gallery", "gallery_shortcode_kandt", 10, 2);
+// We add JS and CSS to the page. This will need to be enhanced to be smarter about 
+// what the client loads into the page. 
+add_action('after_setup_theme', 'setup_kandt_environment' );
+// This is our processing step where we analyze post content on publish.
+add_action('transition_post_status', 'save_shortcode_on_publish', 10,3);
+
+// Collect the first gallery shortcode so it can be used in an excerpt.
+function save_shortcode_on_publish($new_status, $old_status, $post)
+{
+    if ($new_status == "publish")
+    {       
+        //Grab our shortcode if it exists
+        $rgx = "(\[(\s*?)gallery [^\]]*\])";
+        $matches = array();
+        $num = preg_match($rgx, $post->post_content, $matches);
+        if ($num > 0)
+        {
+            for ($inx=0;$inx<$num;$inx++)
+            {
+                // save $matches[$inx] to post meta
+                update_post_meta($post->ID, GALLERY_META_KEY, $matches[$inx]); 
+            }
+        }
+        else 
+        {
+            // No gallery info found, clear any out that might have been saved before.
+            delete_post_meta($post->ID, GALLERY_META_KEY);
+        }    
+    }
+}
+
+// Retrieve the code we set in the above function.
+function get_gallery_shortcode_for_post($post_id)
+{
+    // returns an empty string if no shortcode found.
+    return get_post_meta($post_id, GALLERY_META_KEY, true);
+}
+
+// Build our custom shortcode to force cycling in an excerpt.
+function get_the_cycle_shortcode($id = -1)
+{
+    if ($id < 0) return '';
+    $start = get_gallery_shortcode_for_post($id);
+    if (strlen($start) > 0)
+    {
+        $cycle = ' cycle="true"';
+        $start = substr(trim($start), 0, -1);
+        $start .= $cycle . ']';
+    }
+    return $start;
+}
+
+// Process the shortcode and select the correct output function.
+function gallery_shortcode_kandt($wtf, $attr) {
+    // $attr is the list of attributes and values on the shortcode. keys are attribute names, values are values.
+    // $wtf is always an empty string. I'm not sure why. 
+
+    // If this is feed content, just get out. 
+    if ( is_feed() ) {
+        return "\n";
+    }
+
+    // First, grab the post in which we're rendering.    
+    $post = get_post();
+
+    // Keep track of multiple instances on a single rendering. 
+    static $instance = 0;
+    $instance++;
+
+    // We're trusting author input, so let's at least make sure it looks like a valid orderby statement
+    // I doubt I'll ever use an order by other than that set in the short code. Dump this.
+    if ( isset( $attr['orderby'] ) ) {
+    	$attr['orderby'] = sanitize_sql_orderby( $attr['orderby'] );
+    	if ( !$attr['orderby'] )
+            unset( $attr['orderby'] );
+    }
+
+    // Maybe change logic here to just read the ones from above that I need directly from the array,
+    // then do the extract in the appropriate rendering function. I'll prolly wanna do some logic
+    // that hides the images on a mobile setting, so that we don't download them all on page load. 
+    $id = intval($id);
+    $attachments = array();
+    if ( !empty($attr['include']) ) {
+    	$_attachments = get_posts( array('include' => $attr['include'], 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $attr['order'], 'orderby' => $attr['orderby']) );
+        foreach ( $_attachments as $key => $val ) {
+		$attachments[$val->ID] = $_attachments[$key];
+	}
+    }
+    // Need to return more than just an empty string, else the core shortcode
+    // will proceed with it's original logic, which is something we don't want. 
+    if ( empty($attachments) )
+    	return ' '; 
+	
+    // Here we select an output function. 
+    // If is_single() or is_page() then we do the whole gallery.
+    // Otherwise we're in an excerpt list and we want to just show thumbs in a cycler. 
+    $force_cycle = (isset($attr['cycle']) && strtolower($attr['cycle'])=='true');
+    if($force_cycle || !(is_single() || is_page()) )
+        return output_cycle($attr, $attachments, $instance);
+    else 
+        return output_slideshow($attr, $attachments, $instance);
+}
+
+// If we are in the cycle, we only want thumbs and attachment titles.
+function output_cycle($attr, $attachments, $instance)
+{
+    // Output for cycle are DIV elements with one child IMG element. First DIV is visible,
+    // subsequent children are tagged display:none (or some hidden class).
+    
+    // I'll need different defaults, especially for the elements. 
+    // I can't think of any reason why i'd want anything other than the defaults for most of the other crap.
+    // I won't be doing funky ordering, I'll have custom elements, I won't be doing columns, and I'll have custom logic for sizes. 
+    
+    extract(shortcode_atts(array(
+    	'id'         => $post->ID,
+    	'itemtag'    => 'div',
+	'icontag'    => 'img',
+        'captiontag' => 'div',
+    	'size'       => 'thumbnail',
+        'ids'        => '',
+        'include'    => '',
+        'orderby'    => ''
+    ), $attr));    
+   
+    $i = 0;
+    $output = "<div class=\"cycle_imgs_box\">\n <div class=\"cycle_imgs\">";
+    foreach ( $attachments as $id => $attachment ) {
+    	$link = isset($attr['link']) && 'file' == $attr['link'] ? wp_get_attachment_link($id, $size, false, false) : wp_get_attachment_link($id, $size, true, false); // I don't think I'll ever want the latter here, as I'll always link straight to the image itself in the slideshow. 
+    	// Ok, let's build the output that we need for our slideshow. Change all of this here. 
+	$output .= "<{$itemtag} class='cycle_img' " . ($i++!=0?" style=\"display:none\" ":"") . ">";
+	$output .= $link;
+	$output .= "</{$itemtag}>"; 
+    }
+
+    $output .= "\n </div>\n</div>\n";
+    
+    return $output;
+}
+
+// If we are in full mode, we build the big slideshow. 
+function output_slideshow($attr, &$attachments, $instance)
+{
+    $post = get_post();
+    
+    extract(shortcode_atts(array(
+    	'id'         => $post->ID,
+    	'itemtag'    => 'a',
+	'icontag'    => 'img',
+	'captiontag' => 'div',
+    	'size'       => 'full',
+        'link'       => 'file',
+        'ids'        => '',
+        'include'    => '',
+        'orderby'    => ''
+    ), $attr));
+    
+    $selector = "gallery-{$instance}"; // This is an element ID, not a classname.
+
+    $gallery_style = $gallery_div = ''; 
+    
+    $size_class = sanitize_html_class( $size );
+    $gallery_div = "<div id='$selector' class='gallery galleryid-{$id} gallery-size-{$size_class}'>";
+    $output = "\n\t\t" . $gallery_div; 
+
+    $i = 0;
+    foreach ( $attachments as $id => $attachment ) {
+    	//$link = isset($attr['link']) && 'file' == $attr['link'] ? wp_get_attachment_link($id, $size, false, false) : wp_get_attachment_link($id, $size, true, false); // I don't think I'll ever want the latter here, as I'll always link straight to the image itself in the slideshow. 
+        $link = wp_get_attachment_image_src($id);
+        $linkFull = wp_get_attachment_image_src($id, 'full');
+    	// Ok, let's build the output that we need for our slideshow. Change all of this here. 
+        $caption = trim($attachment->post_content);
+        if ($caption) {
+            $caption = htmlspecialchars($caption, ENT_NOQUOTES | ENT_HTML401, 'UTF-8', false);
+	}
+	$output .= "<{$itemtag} class='gallery-thumbnail' href='" . ($linkFull ? $linkFull[0] : "/wordpress/noimg") ."' data-caption='$caption'>";
+	$output .= "
+            <{$icontag} class='gallery-content' src='" .
+                ($link ? $link[0] : "/wordpress/noimg")
+            ."'></{$icontag}>";
+	
+	$output .= "</{$itemtag}>"; 
+    }
+
+    $output .= "
+	<br style='clear: both;' />
+        </div>\n";
+    
+    return $output;    
+}
+
+// Register our CSS and JS for inclusion in the finished page.
+function setup_kandt_environment()
+{
+    // We should definitely register here
+    wp_register_style('kandtcss', plugins_url('css/kandt.css', __FILE__));
+    wp_register_style('tosruscss', plugins_url('css/jquery.tosrus.css', __FILE__));
+    wp_register_script('cycle.lite', plugins_url('js/jquery.cycle.lite.js', __FILE__), array('jquery'));
+    wp_register_script('hammer', plugins_url('js/jquery.hammer.min.js', __FILE__), array('jquery'));
+    wp_register_script('flameportviewscale', plugins_url('js/FlameViewportScale.js', __FILE__), array('jquery'));
+    wp_register_script('tosrusjs', plugins_url('js/jquery.tosrus-1.3.1-packed.js', __FILE__), array('hammer','flameportviewscale'));    
+    wp_register_script('kandtjs', plugins_url('js/kandt.js', __FILE__), array('jquery'), false, true);
+    
+    // TODO: But we might be able to hold off on enqueuing until we're in output function. Try that.
+    wp_enqueue_style('kandtcss');
+    wp_enqueue_style('tosruscss');
+    wp_enqueue_script('cycle.lite');
+    wp_enqueue_script('tosrusjs');
+    wp_enqueue_script('kandtjs');
+}
+?>
